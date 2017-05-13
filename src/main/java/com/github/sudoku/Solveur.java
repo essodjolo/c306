ee481a6fb2a0eae6aae6506fecf62f0bcdddacb5
Essodjolo KAHANAM <k.essodjolo@gmail.com>
2017-05-13 09:45:03 +0000

    Mise à jour (interface solveur, packages infos & TestSolveur)

--- src/main/java/com/github/sudoku/Solveur.java
+++ src/main/java/com/github/sudoku/Solveur.java
@@ -6,43 +6,25 @@ package com.github.sudoku;
  */
 public interface Solveur {
     /**
-     * puzzle: le plateau de jeu à résoudre.
-     */
-    private GrilleImpl puzzle; 
-
-    /**
-     * constructeur de classe.
-     */
-    public Solveur(GrilleImpl grille) {
-        this.puzzle = grille;
-    }
-
-    /**
      * Teste si le puzzle est un puzzle valide.
      *
+     * @param grille la grille de jeu
      * @return boolean
      */
-    public boolean verifierPuzzle(GrilleImpl grille) {
-        
-    }
+    public boolean verifierPuzzle(GrilleImpl grille);
 
     /**
      * Résoud le puzzle passé en paramètre.
      *
-     * @throws Exception si le puzzle à résoudre n'est pas valable
-     * @throws Exception si aucune solution n'a pu être calculée
+     * @throws  Exception si le puzzle à résoudre n'est pas valable ou 
+     *          si aucune solution n'a pu être calculée
+     * @param grille le puzzle à résoudre
+     * @return une grille bien remplie
      */
-    public char[][] resoudre(GrilleImpl grille) throws Exception {
-        
-    }
+    public char[][] resoudre(GrilleImpl grille) throws Exception;
 
     /**
      * Affiche la solution trouvé au puzzle.
-     *
-     * @throws Exception si le puzzle à résoudre n'est pas valable
-     * @throws Exception si aucune solution n'a pu être calculée
      */
-    public void afficherSolution() {
-        
-    }
+    public void afficherSolution();
 }
--- src/test/java/com/github/sudoku/package-info.java
+++ src/test/java/com/github/sudoku/package-info.java
@@ -1,7 +1,7 @@
 /**
 * tabalgos regroupe un ensemble de méthodes utiles pour faire des traitements
 * basics sur les tableaux.
-* @author kafodome@gmail.com (AFODOME Kwami A.)
+* @author kafodome@gmail.com (AFODOME Kwami A.) & Essodjolo KAHANAM
 */
 package com.github.sudoku;
 
