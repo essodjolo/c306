f9bd945dc476f0611cf9c5c42941c6bb376f15b9
AFODOME Kwami Anukana <kafodome@gmail.com>
2017-05-13 10:58:16 +0000

    Update ISloveur, SolveurImpl.

--- src/main/java/com/github/sudoku/ISolveur.java
+++ src/main/java/com/github/sudoku/ISolveur.java
@@ -8,20 +8,18 @@ public interface ISolveur {
     /**
      * Teste si le puzzle est un puzzle valide.
      *
-     * @param grille la grille de jeu
      * @return boolean
      */
-    public boolean verifierPuzzle(GrilleImpl grille);
+    public boolean verifierPuzzle();
 
     /**
      * Résoud le puzzle passé en paramètre.
      *
      * @throws  Exception si le puzzle à résoudre n'est pas valable ou 
      *          si aucune solution n'a pu être calculée
-     * @param grille le puzzle à résoudre
      * @return une grille bien remplie
      */
-    public char[][] resoudre(GrilleImpl grille) throws Exception;
+    public char[][] resoudre() throws Exception;
 
     /**
      * Affiche la solution trouvé au puzzle.
--- src/main/java/com/github/sudoku/SolveurImpl.java
+++ src/main/java/com/github/sudoku/SolveurImpl.java
@@ -6,16 +6,52 @@ package com.github.sudoku;
  */
 public class SolveurImpl implements ISolveur {
 
-    public boolean verifierPuzzle(GrilleImpl grille) {
+    private GrilleImpl grille;
+
+    /**
+     * constructeur de la classe.
+     *
+     * @param grille
+     */
+    public SolveurImpl(GrilleImpl grille) {
+        this.grille = grille;
+    }
+
+    /**
+     * constructeur par défaut de la classe.
+     *
+     */
+    public SolveurImpl() {
+    }
+
+    /**
+     * Accesseur de la propriété grille SolveurImpl.
+     *
+     * @return GrilleImpl
+     */
+    public GrilleImpl getGrille() {
+        return grille;
+    }
+
+    /**
+     * Mutateur de la propriété gille.
+     *
+     * @param grille
+     */
+    public void setGrille(GrilleImpl grille) {
+        this.grille = grille;
+    }
+
+    public boolean verifierPuzzle() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public char[][] resoudre(GrilleImpl grille) throws Exception {
+    public char[][] resoudre() throws Exception {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public void afficherSolution() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
